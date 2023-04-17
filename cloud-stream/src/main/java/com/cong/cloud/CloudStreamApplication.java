462d9c83cca1b4b11d5c4460d78d9acab7678531
Andy <jiancong.liu@outlook.com>
2023-04-17 22:45:50 +0800

    init commit

--- cloud-stream/src/main/java/com/cong/cloud/CloudStreamApplication.java
+++ cloud-stream/src/main/java/com/cong/cloud/CloudStreamApplication.java
@@ -2,12 +2,38 @@ package com.cong.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.function.Consumer;
 
 @SpringBootApplication
 public class CloudStreamApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CloudStreamApplication.class, args);
-	}
+    @Bean
+    public Consumer<Person> log() {
+        return person -> {
+            System.out.println("Received: " + person);
+        };
+    }
+
+    public static class Person {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String toString() {
+            return this.name;
+        }
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudStreamApplication.class, args);
+    }
 
 }
