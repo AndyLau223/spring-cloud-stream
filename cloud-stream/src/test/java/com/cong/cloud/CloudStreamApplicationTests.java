e3bda6a6f47008bda8b1d514604b6ad64988f4ca
Andy <jiancong.liu@outlook.com>
2023-04-17 22:37:54 +0800

    init commit

---
+++ cloud-stream/src/test/java/com/cong/cloud/CloudStreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cong.cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudStreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
